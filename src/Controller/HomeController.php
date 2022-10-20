7496f3a5072fce16f0c97a8cdeb19b71f048307a
bibimariem <koubaa.mariem@gmail.com>
2022-10-20 21:09:34 +0200

    I applied CSS in addition to the bootstrap and I added the image block with titles

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,6 +9,27 @@ class HomeController extends AbstractController
      */
     public function index(): string
     {
-        return $this->twig->render('Home/index.html.twig');
+        $errors = array();
+        if ($_POST) {
+            //strart validation
+            if (empty($_POST['adress'])) {
+                $errors['adress1'] = "Votre adresse est requise";
+            }
+
+            if (empty($_POST['date'])) {
+                $errors['date1'] = "Votre date de réservation est requise";
+            }
+
+            if (empty($_POST['hour'])) {
+                $errors['hour1'] = "Votre heure de réservation souhétée est obligatoire";
+            }
+
+            if (empty($_POST['benefit'])) {
+                $errors['benefit1'] = "Veuillez choisir un service";
+            }
+        }
+        return $this->twig->render('Home/index.html.twig', [
+            'errors' => $errors
+        ]);
     }
 }
