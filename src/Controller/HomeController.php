1aa36677dda52e58cca820c4464ab2fd26cc5c02
bibimariem <koubaa.mariem@gmail.com>
2022-10-20 21:23:33 +0200

    Update HomeController.php

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,8 +10,7 @@ class HomeController extends AbstractController
     public function index(): string
     {
         $errors = array();
-        if ($_POST) {
-            //strart validation
+        if ($_POST) {//strart validation
             if (empty($_POST['adress'])) {
                 $errors['adress1'] = "Votre adresse est requise";
             }
