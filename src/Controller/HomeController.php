05f84a58bc9de2dab1821188db32ac49c7c5f56a
bibimariem <koubaa.mariem@gmail.com>
2022-10-21 11:31:58 +0200

    changes requested Feature/3 create form home page +  deletion of modifications in class HomeController extends AbstractController to avoid futures conflicts

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,27 +9,6 @@ class HomeController extends AbstractController
      */
     public function index(): string
     {
-        $errors = array();
-        if ($_POST) {
-            //strart validation
-            if (empty($_POST['adress'])) {
-                $errors['adress1'] = "Votre adresse est requise";
-            }
-
-            if (empty($_POST['date'])) {
-                $errors['date1'] = "Votre date de réservation est requise";
-            }
-
-            if (empty($_POST['hour'])) {
-                $errors['hour1'] = "Votre heure de réservation souhaitée est obligatoire";
-            }
-
-            if (empty($_POST['benefit'])) {
-                $errors['benefit1'] = "Veuillez choisir un service";
-            }
-        }
-        return $this->twig->render('Home/index.html.twig', [
-            'errors' => $errors
-        ]);
+        return $this->twig->render('Pages/index.html.twig');
     }
 }
