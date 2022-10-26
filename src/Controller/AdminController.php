5eaf833001aa66bc5bbc4fd615ebc0a12aa121d4
Iancek <arthuriancek@orange.fr>
2022-10-26 14:39:50 +0200

    CRUD on tags finished --> test validations in to do list

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,13 +2,158 @@
 
 namespace App\Controller;
 
+use App\Model\AbstractManager;
+use App\Model\MenuManager;
+use App\Model\TagManager;
+use App\Model\CookManager;
+use App\Model\BookingManager;
+
 class AdminController extends AbstractController
 {
     /**
      * Display interface administrator page
      */
-    public function admin(): string
+    public function indexAdmin(): string
     {
-        return $this->twig->render('Admin/admin.html.twig');
+        $menuManager = new MenuManager();
+        $tagManager = new TagManager();
+        $menus = $menuManager->selectAll();
+        $tags = $tagManager->selectAll();
+        foreach ($menus as $idx => $menu) {
+            $tagsFromMenu = $tagManager->selectAllTagsFromMenu($menu['id']);
+            $menus[$idx]["tags"] = $tagsFromMenu;
+        }
+        $data = ['menus' => $menus];
+        $data += ['tags' => $tags];
+
+        return $this->twig->render('Admin/admin.html.twig', $data);
+    }
+
+    /**
+     * Method for choosing the correct table for CRUD
+     */
+    public function chooseTable(string $table)
+    {
+        switch ($table) {
+            case '0':
+                $manager = new MenuManager();
+                break;
+            case '1':
+                $manager = new TagManager();
+                break;
+            case '2':
+                $manager = new CookManager();
+                break;
+            case '3':
+                $manager = new BookingManager();
+                break;
+            default:
+                $manager = 'failed';
+                break;
+        }
+        return $manager;
+    }
+
+    /**
+     * Method for choosing the correct path for CRUD
+     */
+    public function chooseReturn(string $table)
+    {
+        switch ($table) {
+            case '0':
+                $path = 'Menu.html.twig';
+                break;
+            case '1':
+                $path = 'Tag.html.twig';
+                break;
+            case '2':
+                $path = 'Cook.html.twig';
+                break;
+            case '3':
+                $path = 'Booking.html.twig';
+                break;
+            default:
+                $path = 'failed';
+                break;
+        }
+        return $path;
+    }
+
+    /**
+     * Show informations for a specific item
+     */
+    // public function show(int $id, string $table): string
+    // {
+    //     $manager = $this->chooseTable($table);
+    //     $data = $manager->selectOneById($id);
+
+    //     return $this->twig->render('Admin/admin.html.twig', ['menu' => $data]);
+    // }
+
+    /**
+     * Add a new item : item will be choosed in this method between Tag / Menu / Cook / booking
+     */
+    public function add(string $table): ?string
+    {
+        //Cherche le chemin correct pour déplacer les data en front-end
+        $path = $this->chooseReturn($table);
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // clean $_POST data
+            $item = array_map('trim', $_POST);
+
+            // TODO validations (length, format...) AND IF NULL OR NOT
+
+            // if validation is ok, insert and redirection
+            $manager = $this->chooseTable($table);
+            $manager->insert($item);
+
+            header('Location:/admin');
+            return null;
+        }
+        return $this->twig->render('Item/add' . $path);
+    }
+
+    /**
+     * Edit a specific item : item will be choosed in this method between Tag / Menu / Cook / booking
+     */
+    public function edit(int $id, string $table): ?string
+    {
+        //Cherche la table pour modifier les bonnes data
+        $manager = $this->chooseTable($table);
+        //Cherche le chemin correct pour déplacer les data en front-end
+        $path = $this->chooseReturn($table);
+
+        $item = $manager->selectOneById($id);
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // clean $_POST data
+            $item = array_map('trim', $_POST);
+
+            // TODO validations (length, format...) AND IF NULL OR NOT
+
+            // if validation is ok, update and redirection
+            $manager->update($item);
+
+            header('Location:/admin');
+
+            // we are redirecting so we don't want any content rendered
+            return null;
+        }
+        return $this->twig->render('Item/edit' . $path, [$manager::TABLE => $item]);
+    }
+
+    /**
+     * Delete a specific item : item will be choosed in this method between Tag / Menu / Cook / booking
+     */
+    public function delete(string $table): void
+    {
+        //Cherche la table pour modifier les bonnes data
+        $manager = $this->chooseTable($table);
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $id = trim($_POST['id']);
+            $manager->delete((int)$id);
+            header('Location:/admin');
+        }
     }
 }
--- src/Model/TagManager.php
+++ src/Model/TagManager.php
@@ -7,6 +7,7 @@ use PDO;
 class TagManager extends AbstractManager
 {
     public const TABLE = 'tag';
+    public const TABLEJOIN = 'menu_tag';
 
     /**
      * Get all tags for one menu from database by ID.
@@ -22,5 +23,43 @@ class TagManager extends AbstractManager
         $statement->bindValue('id', $id, \PDO::PARAM_INT);
         $statement->execute();
         return $statement->fetchAll();
+    }
+
+    /**
+     * Insert new tag in database
+     */
+    public function insert(array $tag): int
+    {
+        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (`name_tag`) VALUES (:name_tag)");
+        $statement->bindValue('name_tag', $tag['name_tag'], PDO::PARAM_STR);
+
+        $statement->execute();
+        return (int)$this->pdo->lastInsertId();
+    }
+
+    /**
+     * Update name tag in database
+     */
+    public function update(array $tag): bool
+    {
+        $statement = $this->pdo->prepare("UPDATE " . self::TABLE . " SET `name_tag` = :name_tag 
+        WHERE id=:id");
+        $statement->bindValue('id', $tag['id'], PDO::PARAM_INT);
+        $statement->bindValue('name_tag', $tag['name_tag'], PDO::PARAM_STR);
+
+        return $statement->execute();
+    }
+
+    /**
+     * Overwritting delete method in abstract manager --> deleting lines in table join in more
+     */
+    public function delete(int $id): void
+    {
+        $statement = $this->pdo->prepare("DELETE FROM " . self::TABLEJOIN . " WHERE id_tag=:id");
+        $statement->bindValue('id', $id, \PDO::PARAM_INT);
+        $statement->execute();
+        $statement = $this->pdo->prepare("DELETE FROM " . self::TABLE . " WHERE id=:id");
+        $statement->bindValue('id', $id, \PDO::PARAM_INT);
+        $statement->execute();
     }
 }
